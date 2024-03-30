a96e04a414b0f1e821e3e38724cd1fd747a9ef7d
Erick <erickaug13@hotmail.com>
2024-03-30 20:22:44 -0300

    Created a todo app project which can add and remove task

---
+++ project/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import ThemeProvider from 'react-bootstrap/ThemeProvider'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import "./styles.css"
+
+function App() {
+  const [itemList , setItemList] = useState([]);
+  const [inputText , setInputText] = useState("");
+
+  function handleAdicionaItem(){
+    const textoSemEspacos = inputText.trim()
+    if(textoSemEspacos === "") return; 
+
+    let newItem = {
+      id: Date.now(),
+      itemList: textoSemEspacos,
+    };
+
+    setItemList([...itemList, newItem]);
+    setInputText("");
+  }
+
+  function handleExcluirItem(item){
+    setItemList((prevList) => prevList.filter((lista) => lista !== item));
+  }
+
+  return (
+    <div className="container">
+      <h1>To-do App</h1>
+      <a className="link" href="http://itseriqq.github.io/portfolio" rel="noreferrer" target="_blank"><h2>Erick.dev</h2></a>
+      <div>
+      <input type="text" placeholder="Qual sua tarefa?" value={inputText} onChange={(e) => setInputText(e.target.value)}/>
+      <button className="botaoAdicionarItem" onClick={handleAdicionaItem}>Add</button>
+    </div>
+
+    <ul className="lista">
+      {itemList.map((item) =>(
+        <li className="item" key={item.id}>
+          {item.itemList} <button className="excluirItem" onClick={() => handleExcluirItem(item)}>X</button>{" "}</li>
+
+      ))}
+    </ul>
+    
+    </div>
+  );
+}
+
+export default App;
