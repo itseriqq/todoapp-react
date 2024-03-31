a96597ab566a11c4103599f7b7db375f495e1be9
Erick <erickaug13@hotmail.com>
2024-03-31 17:42:22 -0300

    Placeholder and list are now centralized

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -27,25 +27,29 @@ function App() {
   }
 
   return (
-    <Container fluid>
+    <Container>
       <div className="forms">
 
       
       <Row>
-        <Col xs={12} className="d-flex justify-content center align-items-center flex-column">
-          <h1 className="mt-4">To-do App</h1>
-          <a className="link" href="http://itseriqq.github.io/portfolio" rel="noreferrer" target="_blank"><h2>Erick.dev</h2></a>
+        <Col xs={12}>
+            <div  className="d-flex justify-content center align-items-center flex-column">
+            <h1 className="mt-4">To-do App</h1>
+            <a className="link" href="http://itseriqq.github.io/portfolio" rel="noreferrer" target="_blank"><h2>Erick.dev</h2></a>
+          </div>
         </Col>
       </Row>
 
-      <Row className="mt-3">
-        <Col xs={8} className="ps-4 pe-0">
+      <Row >
+        <Col xs={6} sm={8} md={8} lg={8} className="ps-4 pe-0">
           <div className="w-100">
             <input className="w-100 tarefa" type="text" placeholder="Qual sua tarefa?" value={inputText} onChange={(e) => setInputText(e.target.value)} />
           </div>
         </Col>
-        <Col xs={4} className="ps-3 pe-0">
-          <button className="botaoAdicionarItem" onClick={handleAdicionaItem}>Add</button>
+        <Col xs={6} sm={4} md={4} lg={4}>
+          <div className="pe-0 pe-4">
+            <button className="botaoAdicionarItem" onClick={handleAdicionaItem}>Add</button>
+          </div>
         </Col>
       </Row>
 
@@ -55,7 +59,7 @@ function App() {
             <ul className= "lista">
               {itemList.map((item) => (
                 <Row>
-                  <Col p-0  xs={12}>
+                  <Col xs={12} className="pe-4 ps-4">
                     <li className="item" key={item.id}>
                       {item.itemList} <button className="excluirItem" onClick={() => handleExcluirItem(item)}>X</button>{" "}</li>
                   </Col>
